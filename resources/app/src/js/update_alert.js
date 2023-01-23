12fa17ea0cb46328ce82a1d2513f63a4a4c1ad7a
gaon12 <gokirito12@gmail.com>
2023-01-23 20:14:36 +0900

    1.2.0 업데이트

--- resources/app/src/js/update_alert.js
+++ resources/app/src/js/update_alert.js
@@ -1,41 +1,15 @@
-window.onload = function () {
-  if(localStorage.getItem("first_run") == null) {
-    let timerInterval;
-    Swal.fire({
-        icon: "info",
-        title: "프로그램 업데이트 완료!",
-        html: "업데이트 내역은 프로그램 하단의<br>업데이트 내역에서 확인할 수 있습니다.<br><br>본 창은 <b></b> 밀리초 뒤에 자동으로 닫아집니다.",
-        timer: 5000,
-        timerProgressBar: true,
-        didOpen: () => {
-        Swal.showLoading();
-        const b = Swal.getHtmlContainer().querySelector("b");
-        timerInterval = setInterval(() => {
-            b.textContent = Swal.getTimerLeft();
-        }, 100);
-        },
-        willClose: () => {
-        clearInterval(timerInterval);
-        },
-    }).then((result) => {
-        /* Read more about handling dismissals below */
-        if (result.dismiss === Swal.DismissReason.timer) {
-        console.log("I was closed by the timer");
-        }
-    });
-
-    localStorage.setItem("first_run", "yes");
-  }
-};
-
 function check_update() {
-  let current_version = "1.1.0";
+  let current_version = "1.2.0";
   if (current_version == latest_version) {
     let timerInterval;
     Swal.fire({
       icon: "success",
-      title: "최신버전을 사용중입니다!",
-      html: "현재 <b>최신버전</b>(" + current_version + ")을 사용 중입니다.<br><br>본 창은 <b><cd></cd></b> 밀리초 뒤에 자동으로 닫아집니다.",
+      title: no_update_available,
+      html:
+        no_update_available_text1 +
+        current_version +
+        no_update_available_text2 +
+        no_update_available_text3,
       timer: 5000,
       timerProgressBar: true,
       didOpen: () => {
@@ -56,21 +30,17 @@ function check_update() {
     });
   } else {
     Swal.fire({
-      title: "최신버전이 아닙니다!",
-      text: "최신버전 다운로드를 위한 깃허브 링크를 열까요?",
+      title: update_available,
+      text: update_available_text1 + latest_version + update_available_text2,
       icon: "warning",
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
-      confirmButtonText: "열기",
-      cancelButtonText: "팝업 닫기",
+      confirmButtonText: open_newtab,
+      cancelButtonText: close_popup,
     }).then((result) => {
       if (result.isConfirmed) {
-        var open_github_new_window = window.open(
-          "https://github.com/gaon12/Ranick2-ko/releases",
-          "",
-          "_blank"
-        );
+        var open_github_new_window = window.open("https://github.com/gaon12/Ranick2-ko/releases","","_blank");
       }
     });
   }
